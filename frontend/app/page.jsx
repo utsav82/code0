8337560ddc70315e7b4979c7b49e91abf5c51d1f
utsav82 <utsa.yadav.94@gmail.com>
2024-03-19 20:23:41 +0530

    frontend update

--- frontend/app/page.jsx
+++ frontend/app/page.jsx
@@ -9,8 +9,11 @@ const page = () => {
   return (
     <>
       <div className="container h-full flex flex-col mb-5">
-        <div className="container flex flex-col items-start justify-between space-y-2 py-4 sm:flex-row sm:items-center sm:space-y-0 md:h-16">
+        <div className="flex flex-col items-start justify-between space-y-2 py-4 sm:flex-row sm:items-center sm:space-y-0 md:h-16">
           <h2 className="text-lg font-semibold">Submissions</h2>
+          <p className="text-sm font-thin">
+             Click each entry ID to view its submission 
+          </p>
         </div>
         <Separator />
         <Submissions />
